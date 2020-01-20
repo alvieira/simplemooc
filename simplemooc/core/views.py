b68182502ae4b93982a0615854c77598426dc0fe
Aloisio Vieira <aloisio.vieira@gmail.com>
2020-01-19 21:00:36 -0300

    página de contato

--- simplemooc/core/views.py
+++ simplemooc/core/views.py
@@ -4,4 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html', {'usuario':'Fulano de Tal'})
+    return render(request, 'home.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
--- simplemooc/urls.py
+++ simplemooc/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from simplemooc.core import views
 
 urlpatterns = [
-    path('', views.home, name='home'), 
+    path('', views.home, name='home'),
+    path('contato/', views.contact, name='contact'), 
     path('admin/', admin.site.urls),
 ]
